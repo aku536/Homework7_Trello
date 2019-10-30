053c272ac5f45d9ffaf70001f1c594f66dd85c49
Kirill Afonin <aku536@mail.ru>
2019-10-30 13:21:37 +0300

    Initial Commit

---
+++ Homework7_Trello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework7_Trello
+//
+//  Created by Кирилл Афонин on 30/10/2019.
+//  Copyright © 2019 Кирилл Афонин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
